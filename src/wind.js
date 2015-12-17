be6948ff8d41ab0c718929cbf74b3827cf4cf9f4
rista404 <risticn6@gmail.com>
2015-12-17 23:24:17 +0100

    v0.2

--- src/wind.js
+++ src/wind.js
@@ -12,9 +12,10 @@ var DEFAULTS = {
 
 function Wind(element, options) {
 
-	this.target = element;
-
+	if( !element ) throw new TypeError('Please suply an element.')
 	if( !options ) var options = {};
+
+	this.target = element;
 
 	this.x = options.x || DEFAULTS.x;
 	this.y = options.y || DEFAULTS.y;
@@ -24,24 +25,62 @@ function Wind(element, options) {
 
 	this.onMouseMove = this.onMouseMove.bind(this);
 	this.moveTarget = this.moveTarget.bind(this);
+	this.calculateMovement = this.calculateMovement.bind(this);
 	this.matrix = this.matrix.bind(this);
 	this.pos3d = this.pos3d.bind(this);
-	this.listen();
+	this.init = this.init.bind(this);
+
+
+	this.init();
 
 }
+
+Wind.prototype.init = function(){
+
+	// Checks if NodeList is passed
+	if( this.isNodeList(this.target) ) {
+		// If it is, converts it to an array
+		this.target = Array.prototype.slice.call(this.target);
+	}
+	else {
+		this.target = [this.target];
+	}
+
+	// Checks if rot3d option is passed
+	if(this.rot3d) {
+		this.styling = this.pos3d;
+	}
+	else {
+		this.styling = this.matrix;
+	}
+
+
+	this.listen();
+};
 
 Wind.prototype.listen = function(argument){
 	window.addEventListener('mousemove', this.onMouseMove);
 };
 
+// Determines if passed object is a Node List
+Wind.prototype.isNodeList = function(nodes){
+	var stringRepr = Object.prototype.toString.call(nodes);
+
+	return typeof nodes === 'object' &&
+		/^\[object (HTMLCollection|NodeList|Object)\]$/.test(stringRepr) &&
+		(nodes.length === 0 || (typeof nodes[0] === "object" && nodes[0].nodeType > 0));
+};
+
+// Handles mouse move
 Wind.prototype.onMouseMove = function(event){
 
 	var position = this.getMousePos(event);
+	var movement = this.calculateMovement(position);
 
-	this.moveTarget(position);
+	this.moveTarget(movement);
 };
 
-
+// Gets the x and y coordinates of the cursor
 Wind.prototype.getMousePos = function(event) {
 	var posX = 0;
 	var posY = 0;
@@ -63,16 +102,26 @@ Wind.prototype.getMousePos = function(event) {
 	}
 };
 
+// Calculates the needed movement
+Wind.prototype.calculateMovement = function(position){
+	return {
+		x: 2 * this.x / screen.height * position.y - this.x,
+		y: 2 * this.y / screen.width * position.x - this.y
+	}
+};
 
-Wind.prototype.moveTarget = function(position) {
-	// transform values
-	var rotX = 2 * this.x / screen.height * position.y - this.x,
-		rotY = 2 * this.y / screen.width * position.x - this.y;
+// Applies the styling to move the target
+Wind.prototype.moveTarget = function(movement) {
 
-	this.target.style.WebkitTransform = this.target.style.transform = this.rot3d ? this.pos3d(rotX, rotY) : this.matrix(rotX, rotY);
+	var styling = this.styling(movement.x, movement.y)
+
+	this.target.forEach(function(element, index){
+		element.style.WebkitTransform = element.style.transform = styling;
+	});
 
 };
 
+// Returns a styling for 2d transforms
 Wind.prototype.matrix = function(x, y) {
 	if(this.invert) {
 		var y = -y;
@@ -81,6 +130,7 @@ Wind.prototype.matrix = function(x, y) {
 	return 'matrix(1, 0, 0, 1,' + y + ', ' + x + ')';
 };
 
+// Returns a styling for 3d transforms
 Wind.prototype.pos3d = function(x, y) {
 	if(this.invert) {
 		var y = -y;
@@ -89,7 +139,6 @@ Wind.prototype.pos3d = function(x, y) {
 	return 'rotate3d(1, 0, 0,' + x + 'deg) rotate3d(0, 1, 0,' + y + 'deg)';
 	
 };
-
 
 // transport
 if ( typeof define === 'function' && define.amd ) {
